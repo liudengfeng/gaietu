9384219c576be4c6c870d890e1efb1c29d0901bb
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-17 18:27:05 +0800

    Fix import statement for AudioDataStream class

--- mypylib/azure_speech.py
+++ mypylib/azure_speech.py
@@ -70,7 +70,7 @@ def synthesize_speech_to_stream(
         speech_config=speech_config, audio_config=None
     )
     result = speech_synthesizer.speak_text_async(text).get()
-    stream = speechsdk.audio.AudioDataStream(result)
+    stream = speechsdk.AudioDataStream(result)
     return stream
 
 
