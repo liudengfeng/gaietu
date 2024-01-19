e93e2434e5c266eb376ce449fd4dbadcba2a55a8
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-20 02:55:03 +0800

    Update flashcard auto play button key

--- pages/12_📚_单词.py
+++ pages/12_📚_单词.py
@@ -932,7 +932,7 @@ if menu and menu.endswith("闪卡记忆"):
     )
     auto_play_btn = btn_cols[5].button(
         "轮播[:arrow_forward:]",
-        key="flashcard-play",
+        key="flashcard-auto-play",
         help="✨ 自动单词轮播",
         disabled=len(st.session_state["flashcard-words"]) == 0,
     )
