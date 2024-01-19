c0cc3e138c4b0415f528093db90481a59e738d92
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-19 12:53:09 +0800

    Refactor code for displaying and translating sentences in different languages

--- pages/13_💪_练习.py
+++ pages/13_💪_练习.py
@@ -133,22 +133,22 @@ def process_and_play_dialogue(content_cols, m_voice_style, fm_voice_style, diffi
     voice_style = m_voice_style if idx % 2 == 0 else fm_voice_style
     result = get_synthesis_speech(sentence, voice_style[0])
 
-    content_cols[1].audio(result["audio_data"], format="audio/wav")
 
     if st.session_state["ls-display-state"] == "英文":
         content_cols[0].markdown("英文")
         content_cols[0].markdown(sentence)
     elif st.session_state["ls-display-state"] == "中文":
         cn = translate_text(sentence, "zh-CN")
-        content_cols[2].markdown("中文")
-        content_cols[2].markdown(cn)
+        content_cols[1].markdown("中文")
+        content_cols[1].markdown(cn)
     else:
         content_cols[0].markdown("英文")
         content_cols[0].markdown(sentence)
         cn = translate_text(sentence, "zh-CN")
-        content_cols[2].markdown("中文")
-        content_cols[2].markdown(cn)
+        content_cols[1].markdown("中文")
+        content_cols[1].markdown(cn)
 
+    content_cols[0].audio(result["audio_data"], format="audio/wav")
     # 记录学习时长
     if len(st.session_state["learning-record"]) > 0:
         st.session_state["learning-record"][-1].end()
@@ -448,7 +448,7 @@ if menu is not None and menu.endswith("听说练习"):
             or st.session_state["ls-idx"] == len(st.session_state.conversation_scene) - 1,  # type: ignore
         )
 
-        content_cols = st.columns([2, 1, 2])
+        content_cols = st.columns(2)
 
         if refresh_btn:
             st.session_state["ls-idx"] = -1
