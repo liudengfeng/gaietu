48e950ff9825c2315fccf706c657f2999198da61
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-19 17:08:30 +0800

    Remove sleep statement in code

--- pages/13_💪_练习.py
+++ pages/13_💪_练习.py
@@ -695,7 +695,7 @@ if menu is not None and menu.endswith("听说练习"):
             question_audio = get_synthesis_speech(question, m_voice_style[0])
             audio_html = audio_autoplay_elem(question_audio["audio_data"], fmt="wav")
             st.markdown(audio_html, unsafe_allow_html=True)
-            time.sleep(question_audio["audio_duration"].total_seconds())
+            # time.sleep(question_audio["audio_duration"].total_seconds())
 
             # 添加一个学习时间记录
             record = LearningTime(
