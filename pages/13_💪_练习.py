74853a1812fb0154ee34d4d5e71c5161e27439a8
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-22 16:17:52 +0800

    Refactor caching logic in generate_scenarios_for function

--- pages/13_💪_练习.py
+++ pages/13_💪_练习.py
@@ -212,7 +212,7 @@ def generate_reading_test_for(difficulty: str, exercise_type, article: List[str]
     )
 
 
-# @st.cache_data(ttl=60 * 60 * 24, show_spinner="正在加载场景类别，请稍候...")
+@st.cache_data(ttl=60 * 60 * 24, show_spinner="正在加载场景类别，请稍候...")
 def generate_scenarios_for(category: str):
     return generate_scenarios(st.session_state["text_model"], category)
 
@@ -760,7 +760,7 @@ if menu is not None and menu.endswith("听说练习"):
                     st.session_state["scenario-list"] = generate_scenarios_for(
                         scenario_category
                     )
-                    st.write(st.session_state["scenario-list"])
+                    # st.write(st.session_state["scenario-list"])
 
                 # st.write(scenario_list)
                 selected_scenario = st.selectbox(
