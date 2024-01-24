8c0106b9a2a0e7cc7b22086767d007882773b0ce
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-24 13:00:55 +0800

    Add question number to test view

--- pages/12_📚_单词.py
+++ pages/12_📚_单词.py
@@ -791,6 +791,7 @@ def on_word_test_radio_change(idx, options):
 def view_test_word(container):
     idx = st.session_state["word-test-idx"]
     test = st.session_state["word-tests"][idx]
+    container.write(f"第 {idx+1} 题 \n{test}")
     question = test["question"]
     options = test["options"]
     user_answer_idx = st.session_state["user-answer"][idx]
