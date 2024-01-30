f4acd6cc48fba43801b90b89a1bb506ed8d8e623
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-31 07:15:17 +0800

    Add token counting for "gemini-pro-vision"

--- pages/29_♊_GAI.py
+++ pages/29_♊_GAI.py
@@ -594,6 +594,7 @@ elif menu == "多模态AI":
             col1, col2 = response_container.columns([1, 1])
             view_example(contents, col1)
             generate_content_from_files_and_prompt(contents, col2.empty())
+            count_tokens("gemini-pro-vision", contents)
 
 # endregion
 
