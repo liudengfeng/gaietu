62a00e1fc7b3e59501c448e9386533c21470aed1
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-30 14:30:16 +0800

    Refactor autoplay_audio_and_display_text function

--- mypylib/st_helper.py
+++ mypylib/st_helper.py
@@ -339,9 +339,10 @@ def autoplay_audio_and_display_text(
     components.html(auto_html)
 
     start_time = time.perf_counter()
-    for accumulated_text, _, offset, _ in get_syllable_durations_and_offsets(words):
-        while time.perf_counter() - start_time < offset:
-            time.sleep(0.001)
+    for accumulated_text, duration, offset, _ in get_syllable_durations_and_offsets(words):
+        time.sleep(duration)
+        # while time.perf_counter() - start_time < offset:
+        #     time.sleep(0.001)
         elem.markdown(accumulated_text + "▌")
     elem.markdown(accumulated_text)
 
