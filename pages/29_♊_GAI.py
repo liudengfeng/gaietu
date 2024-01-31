f896f47479fbedf93bf1e982c4b3404c0c756a08
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-31 15:14:01 +0800

    Refactor display_generated_content_for function

--- pages/29_♊_GAI.py
+++ pages/29_♊_GAI.py
@@ -157,12 +157,15 @@ def process_files_and_prompt(uploaded_files, prompt):
 @st.cache_data
 def display_generated_content_for(model_name, config, contents, stream, placeholder):
     model = load_vertex_model(model_name)
+    generation_config = GenerationConfig(
+        **config,
+    )
     display_generated_content_and_update_token(
         "多模态AI",
         model_name,
         model.generate_content,
         contents,
-        config,
+        generation_config,
         stream=stream,
         placeholder=placeholder,
     )
@@ -618,14 +621,20 @@ elif menu == "多模态AI":
             response_container.empty()
             col1, col2 = response_container.columns([1, 1])
             view_example(contents, col1)
-            generation_config = GenerationConfig(
-                temperature=st.session_state["temperature"],
-                top_p=st.session_state["top_p"],
-                top_k=st.session_state["top_k"],
-                max_output_tokens=st.session_state["max_output_tokens"],
-            )
+            config = {
+                "temperature": st.session_state["temperature"],
+                "top_p": st.session_state["top_p"],
+                "top_k": st.session_state["top_k"],
+                "max_output_tokens": st.session_state["max_output_tokens"],
+            }
             # generate_content_from_files_and_prompt(contents, col2.empty())
-            display_generated_content_for("gemini-pro-vision", generation_config, contents, stream=True, placeholder=col2.empty())
+            display_generated_content_for(
+                "gemini-pro-vision",
+                config,
+                contents,
+                stream=True,
+                placeholder=col2.empty(),
+            )
             sidebar_status.markdown(
                 f"""令牌：{st.session_state.current_token_count} 累计：{format_token_count(st.session_state.total_token_count)}""",
                 help=TOEKN_HELP_INFO,
