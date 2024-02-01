092c1a7cf749e62da0cf4fa075c1ffa6b4c6e07e
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-01 15:36:45 +0800

    Refactor display_grammar_errors function to improve readability and add explanations for replaced words

--- pages/14_🏄‍♀️_写作.py
+++ pages/14_🏄‍♀️_写作.py
@@ -168,12 +168,11 @@ def display_grammar_errors(original, corrected, explanations):
                     f"<ins style='color:blue;text-decoration: underline' title='{explanation}'>{diff[i + 1][2:]}</ins>"
                 )
                 i += 1  # 跳过下一个元素
-            explanation_index += 1
+                explanation_index += 1  # 每个替换后的单词都添加解释
         elif diff[i][0] == "+":
-            if i > 0 and diff[i - 1][0] != "-":
-                result.append(
-                    f"<ins style='color:green;text-decoration: underline' title='{explanation}'>{diff[i][2:]}</ins>"
-                )
+            result.append(
+                f"<ins style='color:green;text-decoration: underline' title='{explanation}'>{diff[i][2:]}</ins>"
+            )
             explanation_index += 1
         else:
             result.append(f"<span>{diff[i][2:]}</span>")
