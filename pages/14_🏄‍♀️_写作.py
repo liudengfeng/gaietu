30def00f5558dd13bcc20e56ad84a433c18ded1d
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-02 06:04:36 +0800

    更新作文文本区域的默认值

--- pages/14_🏄‍♀️_写作.py
+++ pages/14_🏄‍♀️_写作.py
@@ -48,6 +48,9 @@ check_and_force_logout(sidebar_status)
 if "text-model" not in st.session_state:
     st.session_state["text-model"] = load_vertex_model("gemini-pro")
 
+if "writing-text" not in st.session_state:
+    st.session_state["writing-text"] = ""
+
 # endregion
 
 # region 边栏
@@ -131,7 +134,7 @@ w_cols[0].markdown("<h5 style='color: blue;'>您的作文</h5>", unsafe_allow_ht
 text = w_cols[0].text_area(
     "您的作文",
     max_chars=10000,
-    key="writing-area",
+    value=st.session_state["writing-text"],
     height=HEIGHT,
     placeholder="在此输入您的作文",
     help="在此输入您的作文",
