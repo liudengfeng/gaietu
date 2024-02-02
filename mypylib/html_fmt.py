743d77dfa936655b1a61ffdbb4c27a48d33cad0a
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-02 08:12:19 +0800

    更新发音错误计数的样式

--- mypylib/html_fmt.py
+++ mypylib/html_fmt.py
@@ -11,42 +11,42 @@ def view_error_counts_legend(session_state_key: str, idx=None):
 
     n1 = str(d.get("Mispronunciation", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: black; background-color: #F5F5DC; margin-right: 10px;'>{n1}</div> <span>发音错误</span>",
+        f"<div style='text-align: left; color: black; background-color: #F5F5DC; margin-right: 10px;'>{n1}</div> <span>发音错误</span>",
         help="✨ 说得不正确的字词。",
         unsafe_allow_html=True,
     )
 
     n2 = str(d.get("Omission", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: white; background-color: #A9A9A9; margin-right: 10px;'>{n2}</div> <span>遗漏</span>",
+        f"<div style='text-align: left; color: white; background-color: #A9A9A9; margin-right: 10px;'>{n2}</div> <span>遗漏</span>",
         help="✨ 脚本中已提供，但未说出的字词。",
         unsafe_allow_html=True,
     )
 
     n3 = str(d.get("Insertion", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: white; background-color: #B7410E; margin-right: 10px;'>{n3}</div> <span>插入内容</span>",
+        f"<div style='text-align: left; color: white; background-color: #B7410E; margin-right: 10px;'>{n3}</div> <span>插入内容</span>",
         help="✨ 不在脚本中但在录制中检测到的字词。",
         unsafe_allow_html=True,
     )
 
     n4 = str(d.get("UnexpectedBreak", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: black; background-color: #FFC0CB; margin-right: 10px;'>{n4}</div> <span>意外中断</span>",
+        f"<div style='text-align: left; color: black; background-color: #FFC0CB; margin-right: 10px;'>{n4}</div> <span>意外中断</span>",
         help="✨ 同一句子中的单词之间未正确暂停。",
         unsafe_allow_html=True,
     )
 
     n5 = str(d.get("MissingBreak", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: black; background-color: #D3D3D3; margin-right: 10px;'>{n5}</div> <span>缺少停顿</span>",
+        f"<div style='text-align: left; color: black; background-color: #D3D3D3; margin-right: 10px;'>{n5}</div> <span>缺少停顿</span>",
         help="✨ 当两个单词之间存在标点符号时，词之间缺少暂停。",
         unsafe_allow_html=True,
     )
 
     n6 = str(d.get("Monotone", 0)).zfill(3)
     st.markdown(
-        f"<div style='text-align: right; color: white; background-color: #800080; margin-right: 10px;'>{n6}</div> <span>发音单调</span>",
+        f"<div style='text-align: left; color: white; background-color: #800080; margin-right: 10px;'>{n6}</div> <span>发音单调</span>",
         help="✨ 这些单词正以平淡且不兴奋的语调阅读，没有任何节奏或表达。",
         unsafe_allow_html=True,
     )
