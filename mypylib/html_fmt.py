db5b0f6fc3f7b27aff10d81c84f94fe2c8557b49
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-02 09:42:02 +0800

    更新HTML格式化代码

--- mypylib/html_fmt.py
+++ mypylib/html_fmt.py
@@ -66,17 +66,17 @@ def pronunciation_assessment_word_format(word_obj):
 
     if error_type == "Mispronunciation":
         result = f"<span style='color: black; background-color: #F5F5DC; margin-right: 5px; text-decoration: underline;' {underline_style} title='{accuracy_score}'>{word_obj.word}</span>"
-    elif error_type == "Omission":
-        result = f"<span style='color: white; background-color: #808080; margin-right: 5px;' {underline_style}>[{word_obj.word}]</span>"
-    elif error_type == "Insertion":
-        result = f"<span style='color: white; background-color: #B7410E; margin-right: 5px; text-decoration: line-through;' {underline_style} title='{accuracy_score}'>{word_obj.word}</span>"
+    # elif error_type == "Omission":
+    #     result = f"<span style='color: white; background-color: #808080; margin-right: 5px;' {underline_style}>[{word_obj.word}]</span>"
+    # elif error_type == "Insertion":
+    #     result = f"<span style='color: white; background-color: #B7410E; margin-right: 5px; text-decoration: line-through;' {underline_style} title='{accuracy_score}'>{word_obj.word}</span>"
 
-    if word_obj.is_unexpected_break:
-        result += f"<span style='color: black; background-color: #FFC0CB; text-decoration: line-through; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
-        result += f"<span style='color: white; background-color: #B7410E; margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
-    elif word_obj.is_missing_break:
-        result += f"<span style='color: black; background-color: #D3D3D3; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
-        result += f"<span style='margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
+    # if word_obj.is_unexpected_break:
+    #     result += f"<span style='color: black; background-color: #FFC0CB; text-decoration: line-through; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
+    #     result += f"<span style='color: white; background-color: #B7410E; margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
+    # elif word_obj.is_missing_break:
+    #     result += f"<span style='color: black; background-color: #D3D3D3; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
+    #     result += f"<span style='margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
     else:
         result = f"<span style='margin-right: 5px;'>{word_obj.word}</span>"
 
