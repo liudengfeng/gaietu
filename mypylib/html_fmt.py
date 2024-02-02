a8232d6bb2995527049f4c99836bda16fc0c5364
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-02 09:31:15 +0800

    更新发音评估单词格式化函数

--- mypylib/html_fmt.py
+++ mypylib/html_fmt.py
@@ -52,31 +52,31 @@ def view_error_counts_legend(session_state_key: str, idx=None):
     )
 
 
-def pronunciation_assessment_word_format(word):
-    if isinstance(word, str):
-        return f"<span style='margin-right: 5px;'>{word}</span>"
-    error_type = word.error_type
-    accuracy_score = round(word.accuracy_score)
+def pronunciation_assessment_word_format(word_obj):
+    if isinstance(word_obj, str):
+        return f"<span style='margin-right: 5px;'>{word_obj}</span>"
+    error_type = word_obj.error_type
+    accuracy_score = round(word_obj.accuracy_score)
     result = ""
     if error_type == "Mispronunciation":
-        result = f"<span style='color: black; background-color: #F5F5DC; margin-right: 5px; text-decoration: underline;' title='{accuracy_score}'>{word}</span>"
+        result = f"<span style='color: black; background-color: #F5F5DC; margin-right: 5px; text-decoration: underline;' title='{accuracy_score}'>{word_obj.word}</span>"
     elif error_type == "Omission":
-        result = f"<span style='color: white; background-color: #808080; margin-right: 5px;'>[{word}]</span>"
+        result = f"<span style='color: white; background-color: #808080; margin-right: 5px;'>[{word_obj.word}]</span>"
     elif error_type == "Insertion":
-        result = f"<span style='color: white; background-color: #B7410E; margin-right: 5px; text-decoration: line-through;' title='{accuracy_score}'>{word}</span>"
+        result = f"<span style='color: white; background-color: #B7410E; margin-right: 5px; text-decoration: line-through;' title='{accuracy_score}'>{word_obj.word}</span>"
 
-    if word.is_unexpected_break:
+    if word_obj.is_unexpected_break:
         result += f"<span style='color: black; background-color: #FFC0CB; text-decoration: line-through; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
-        result += f"<span style='color: white; background-color: #B7410E; margin-right: 5px;' title='{accuracy_score}'>{word}</span>"
-    elif word.is_missing_break:
+        result += f"<span style='color: white; background-color: #B7410E; margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
+    elif word_obj.is_missing_break:
         result += f"<span style='color: black; background-color: #D3D3D3; margin-right: 5px;' title='{accuracy_score}'>[]</span>"
         result += (
-            f"<span style='margin-right: 5px;' title='{accuracy_score}'>{word}</span>"
+            f"<span style='margin-right: 5px;' title='{accuracy_score}'>{word_obj.word}</span>"
         )
     else:
-        result = f"<span style='margin-right: 5px;'>{word}</span>"
+        result = f"<span style='margin-right: 5px;'>{word_obj.word}</span>"
 
-    if word.is_monotone:
+    if word_obj.is_monotone:
         result = f"<u style='text-decoration: underline; text-decoration-color: purple;'>{result}</u>"
     return result
 
