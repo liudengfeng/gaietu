78f91de891e9273eeabaa4b53064db4220791cce
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-04 05:45:38 +0800

    更新多模态AI页面的侧边栏配置

--- pages/29_♊_GAI.py
+++ pages/29_♊_GAI.py
@@ -309,7 +309,7 @@ if menu == "聊天机器人":
     )
     st.sidebar.divider()
     sidebar_cols = st.sidebar.columns([3, 1])
-    
+
     create_synchronized_components(
         sidebar_cols,
         "max-output-tokens-chatbot",
@@ -587,7 +587,7 @@ elif menu == "多模态AI":
         2048,
         "✨ 词元限制决定了一条提示的最大文本输出量。词元约为 4 个字符。默认值为 2048。",
     )
-    
+
     # st.sidebar.slider(
     #     "词元限制",
     #     key="max-output-tokens-vision",
@@ -597,7 +597,7 @@ elif menu == "多模态AI":
     #     step=16,
     #     help="""✨ 词元限制决定了一条提示的最大文本输出量。词元约为`4`个字符。默认值为`2048`""",
     # )
-    
+
     create_synchronized_components(
         sidebar_cols,
         "temperature-vision",
@@ -626,20 +626,20 @@ elif menu == "多模态AI":
         """✨ `Top-k`可更改模型选择输出词元的方式。
 - 如果`Top-k`设为`1`，表示所选词元是模型词汇表的所有词元中概率最高的词元（也称为贪心解码）。
 - 如果`Top-k`设为`3`，则表示系统将从`3`个概率最高的词元（通过温度确定）中选择下一个词元。
-- 多模态`Top-k`的默认值为`32`。"""
+- 多模态`Top-k`的默认值为`32`。""",
     )
-#     st.sidebar.slider(
-#         "Top K",
-#         key="top-k-vision",
-#         min_value=1,
-#         max_value=40,
-#         value=32,
-#         step=1,
-#         help="""✨ `Top-k`可更改模型选择输出词元的方式。
-# - 如果`Top-k`设为`1`，表示所选词元是模型词汇表的所有词元中概率最高的词元（也称为贪心解码）。
-# - 如果`Top-k`设为`3`，则表示系统将从`3`个概率最高的词元（通过温度确定）中选择下一个词元。
-# - 多模态`Top-k`的默认值为`32`。""",
-#     )
+    #     st.sidebar.slider(
+    #         "Top K",
+    #         key="top-k-vision",
+    #         min_value=1,
+    #         max_value=40,
+    #         value=32,
+    #         step=1,
+    #         help="""✨ `Top-k`可更改模型选择输出词元的方式。
+    # - 如果`Top-k`设为`1`，表示所选词元是模型词汇表的所有词元中概率最高的词元（也称为贪心解码）。
+    # - 如果`Top-k`设为`3`，则表示系统将从`3`个概率最高的词元（通过温度确定）中选择下一个词元。
+    # - 多模态`Top-k`的默认值为`32`。""",
+    #     )
     create_synchronized_components(
         sidebar_cols,
         "top-p-vision",
@@ -649,21 +649,21 @@ elif menu == "多模态AI":
         1.0,
         """✨ `Top-p`可更改模型选择输出词元的方式。系统会按照概率从最高到最低的顺序选择词元，直到所选词元的概率总和等于 Top-p 的值。
 - 例如，如果词元`A`、`B` 和`C`的概率分别是`0.3`、`0.2`和`0.1`，并且`Top-p`的值为`0.5`，则模型将选择`A`或`B`作为下一个词元（通过温度确定）。
-- 多模态`Top-p`的默认值为`1.0`。"""
+- 多模态`Top-p`的默认值为`1.0`。""",
     )
-    
-#     st.sidebar.slider(
-#         "Top P",
-#         key="top-p-vision",
-#         min_value=0.00,
-#         max_value=1.0,
-#         value=1.0,
-#         step=0.05,
-#         help="""✨ `Top-p`可更改模型选择输出词元的方式。系统会按照概率从最高到最低的顺序选择词元，直到所选词元的概率总和等于 Top-p 的值。
-# - 例如，如果词元`A`、`B` 和`C`的概率分别是`0.3`、`0.2`和`0.1`，并且`Top-p`的值为`0.5`，则模型将选择`A`或`B`作为下一个词元（通过温度确定）。
-# - 多模态`Top-p`的默认值为`1.0`。""",
-#     )
-    
+
+    #     st.sidebar.slider(
+    #         "Top P",
+    #         key="top-p-vision",
+    #         min_value=0.00,
+    #         max_value=1.0,
+    #         value=1.0,
+    #         step=0.05,
+    #         help="""✨ `Top-p`可更改模型选择输出词元的方式。系统会按照概率从最高到最低的顺序选择词元，直到所选词元的概率总和等于 Top-p 的值。
+    # - 例如，如果词元`A`、`B` 和`C`的概率分别是`0.3`、`0.2`和`0.1`，并且`Top-p`的值为`0.5`，则模型将选择`A`或`B`作为下一个词元（通过温度确定）。
+    # - 多模态`Top-p`的默认值为`1.0`。""",
+    #     )
+
     st.sidebar.text_input(
         "添加停止序列",
         key="stop_sequences",
