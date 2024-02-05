98ef7861727e1d784231af3bbd9cce98f9130e8d
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-05 10:03:21 +0800

    添加时区转换到UTC时间

--- mypylib/statistics.py
+++ mypylib/statistics.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import streamlit as st
+import pytz
 
 
 @st.cache_data(ttl=60 * 60 * 1)
@@ -11,6 +12,10 @@ def get_exercises(phone_number, start_date, end_date):
     # 将日期转换为 Firestore 时间戳
     start_timestamp = datetime.combine(start_date, datetime.min.time())
     end_timestamp = datetime.combine(end_date, datetime.max.time())
+
+    # 转换为 UTC 时间
+    start_timestamp = pytz.UTC.localize(start_timestamp)
+    end_timestamp = pytz.UTC.localize(end_timestamp)
 
     # 获取指定 ID 的文档
     doc = db.collection("exercises").document(phone_number).get()
@@ -28,6 +33,7 @@ def get_exercises(phone_number, start_date, end_date):
 
             # 获取 "record_time" 字段
             timestamp = record.get("timestamp")
+            timestamp = timestamp.astimezone(pytz.UTC)
             # 如果 "record_time" 字段存在，并且在指定的范围内，则添加到查询结果中
             if timestamp and start_timestamp <= timestamp <= end_timestamp:
                 # 给 record 字典添加 phone_number 键值对
