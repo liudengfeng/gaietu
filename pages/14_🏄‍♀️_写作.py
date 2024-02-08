41e759840f789ffb985515d752631d41b016f71a
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-08 13:29:32 +0800

    更新页面14_问题_答案.py文件中的AI回复格式

--- pages/14_🏄‍♀️_写作.py
+++ pages/14_🏄‍♀️_写作.py
@@ -407,8 +407,10 @@ with w_cols[2]:
                 placeholder=ai_tip_container.empty(),
             )
             st.session_state["writing-ai-assitant"] = (
-                f"您的问题：\n{prompt}"
-                + f'AI回复：\n{st.session_state["writing-chat"].history[-1].parts[0].text}'
+                f"""您的问题：
+                {prompt}
+                'AI回复：
+                {st.session_state["writing-chat"].history[-1].parts[0].text}"""
             )
             update_sidebar_status(sidebar_status)
     ai_tip_container.markdown(st.session_state["writing-ai-assitant"])
