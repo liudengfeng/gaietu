a188570e502c65939c641bba9ca7098c8d9ab8ec
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-11 01:25:09 +0800

    更新项目切换逻辑

--- mypylib/st_helper.py
+++ mypylib/st_helper.py
@@ -744,11 +744,11 @@ def on_project_changed(project_name):
     }
     st.session_state["current-project"] = project_name
 
-    for project, data in st.session_state["project-timer"].items():
-        if "duration" in data:
-            duration_seconds = data["duration"]
-            logger.info(f"项目 {project} 的总时长（秒）: {duration_seconds}")
-    logger.info("=====================================")
+    # for project, data in st.session_state["project-timer"].items():
+    #     if "duration" in data:
+    #         duration_seconds = data["duration"]
+    #         logger.info(f"项目 {project} 的总时长（秒）: {duration_seconds}")
+    # logger.info("=====================================")
 
 
 def add_exercises_to_db(force=False):
