2936c73742c747a30833aa4343ee44e585b14658
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-14 16:48:21 +0800

    更新学习时间显示

--- mypylib/statistics_report.py
+++ mypylib/statistics_report.py
@@ -144,7 +144,8 @@ def display_word_study(
 
     column_config = {
         "学习日期": "学习日期",
-        "学习时间": st.column_config.LineChartColumn("学习时间", y_min=0, y_max=1440),
+        "学习时间": "学习时间",
+        # "学习时间": st.column_config.LineChartColumn("学习时间", y_min=0, y_max=1440),
         "单词数量": st.column_config.LineChartColumn("单词数量", y_min=0, y_max=1000),
     }
     st.dataframe(
