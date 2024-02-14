f2e23167b5adacfc6ec7d83989a832c19031bac6
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-14 11:57:43 +0800

    移动单词之间的容器***
    这个提交包含了移动单词之间容器的改动。

--- mypylib/st_utils.py
+++ mypylib/st_utils.py
@@ -4,21 +4,36 @@ import logging
 logger = logging.getLogger("streamlit")
 
 
-def move_words_between_containers(source_container, target_container, words):
+def move_words_between_containers(
+    source_container, target_container, words, is_char=False
+):
+    """
+    Move words between two containers.
+
+    Args:
+        source_container: The container from which words are moved.
+        target_container: The container to which words are moved.
+        words: The list of words to be moved.
+        is_char: A boolean indicating whether the words are characters.
+
+    Returns:
+        None
+    """
+
     if "source-container-words" not in st.session_state:
         st.session_state["source-container-words"] = words.copy()
 
     if "target-container-words" not in st.session_state:
         st.session_state["target-container-words"] = []
 
-    group_size = 6
+    group_size = 36 if is_char else 6
     n = len(st.session_state["source-container-words"])
     src_cols = source_container.columns(group_size)
     for i in range(n):
         if src_cols[i % group_size].button(
             st.session_state["source-container-words"][i],
             key=f"word-src-{i}",
-            help="✨ 点击按钮，将单词移动到目标位置。",
+            help="✨ Click the button to move the word to the target position.",
             # use_container_width=True,
         ):
             sw = st.session_state["source-container-words"][i]
@@ -32,7 +47,7 @@ def move_words_between_containers(source_container, target_container, words):
         if tgt_cols[i % group_size].button(
             st.session_state["target-container-words"][i],
             key=f"word-tgt-{i}",
-            help="✨ 点击按钮，将单词放回到目标位置。",
+            help="✨ Click the button to move the word back to the target position.",
             # use_container_width=True,
         ):
             tw = st.session_state["target-container-words"][i]
