24ca4a84610f0c6ee68981eb99ddb51a2eae9cfc
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-19 18:22:23 +0800

    更新人工智能页面和功能

--- pages/50_test.py
+++ pages/50_test.py
@@ -4,11 +4,23 @@ import streamlit as st
 from langchain_core.messages import HumanMessage
 from langchain_google_vertexai import ChatVertexAI, HarmBlockThreshold, HarmCategory
 
+from menu import menu
+from mypylib.st_helper import add_exercises_to_db, check_access, configure_google_apis
+
 logger = logging.getLogger("streamlit")
 
+st.set_page_config(
+    page_title="人工智能",
+    page_icon=":gemini:",
+    layout="wide",
+)
+menu()
+check_access(False)
+configure_google_apis()
+add_exercises_to_db()
 
 llm = ChatVertexAI(
-    model="gemini-pro-vision",
+    model_name="gemini-pro-vision",
 )
 
 # example
