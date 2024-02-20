a63a560a3c57b48c6cf985ad250561cc3fb86ead
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-20 11:59:34 +0800

    添加路径导入和图像显示功能

--- pages/50_test.py
+++ pages/50_test.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 import streamlit as st
 from langchain_community.document_loaders import MathpixPDFLoader
@@ -10,10 +11,14 @@ from langchain_google_vertexai import (
     HarmCategory,
     VertexAI,
 )
+from vertexai.preview.generative_models import Image
 from menu import menu
 from mypylib.st_helper import add_exercises_to_db, check_access, configure_google_apis
 
 logger = logging.getLogger("streamlit")
+CURRENT_CWD: Path = Path(__file__).parent.parent
+IMAGE_DIR: Path = CURRENT_CWD / "resource/multimodal"
+
 
 st.set_page_config(
     page_title="人工智能",
@@ -25,11 +30,16 @@ check_access(False)
 configure_google_apis()
 add_exercises_to_db()
 
+img_path = IMAGE_DIR / "math/高中/定积分.png"
+i = Image.load_from_file(str(img_path))
+st.image(i, caption="定积分", use_column_width=True)
 
 llm = ChatVertexAI(model_name="gemini-pro-vision")
 image_message = {
     "type": "image_url",
-    "image_url": {"url": "dog.png"},
+    "image_url": {
+        "url": "https://python.langchain.com/assets/images/cell-18-output-1-0c7fb8b94ff032d51bfe1880d8370104.png",
+    },
 }
 text_message = {
     "type": "text",
