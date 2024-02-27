0998a608d838b50194601ec38e87734158e474a2
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-27 18:53:28 +0800

    更新图像URL生成逻辑

--- pages/31_🧮_数学助手.py
+++ pages/31_🧮_数学助手.py
@@ -199,11 +199,11 @@ def image_to_dict(uploaded_file):
     ext = mimetypes.guess_extension(mime_type)
 
     # logger.info(f"ext: {ext}")
-
+    # "image_url": {"url": f"data:image/jpeg;base64,{image}"},
     # 返回临时文件的路径
     image_message = {
         "type": "image_url",
-        "image_url": {"url": f"data:image/{ext[1:]};base64,{image_bytes}"},
+        "image_url": {"url": f"data:image/jpeg;base64,{image_bytes}"},
     }
     return image_message
 
