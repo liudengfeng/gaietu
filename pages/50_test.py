1d0447833cef6bbeb3928305db73ee62c2a4b5df
liudengfeng <liudengfeng_sd@outlook.com>
2024-02-28 13:14:21 +0800

    更新50_test.py文件，添加了ergion graph的相关代码和函数。

--- pages/50_test.py
+++ pages/50_test.py
@@ -147,11 +147,72 @@ uploaded_file = st.file_uploader(
 text = st.text_input("输入问题")
 
 
-class AgentInput(BaseModel):
-    input: str
-    chat_history: List[Tuple[str, str]] = Field(
-        ..., extra={"widget": {"type": "chat", "input": "input", "output": "output"}}
+# ergion graph
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+
+from langgraph.prebuilt import ToolExecutor
+
+
+import json
+from langchain_core.messages import FunctionMessage
+from typing import TypedDict, Annotated, Sequence
+import operator
+from langchain_core.messages import BaseMessage
+from langgraph.prebuilt import ToolInvocation
+from langgraph.graph import StateGraph, END
+
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+
+tools = [TavilySearchResults(max_results=1)]
+tool_executor = ToolExecutor(tools)
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no function call, then we finish
+    if "function_call" not in last_message.additional_kwargs:
+        return "end"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
+
+
+# Define the function that calls the model
+def call_model(state):
+    messages = state["messages"]
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+# Define the function to execute tools
+def call_tool(state):
+    messages = state["messages"]
+    # Based on the continue condition
+    # we know the last message involves a function call
+    last_message = messages[-1]
+    # We construct an ToolInvocation from the function_call
+    action = ToolInvocation(
+        tool=last_message.additional_kwargs["function_call"]["name"],
+        tool_input=json.loads(
+            last_message.additional_kwargs["function_call"]["arguments"]
+        ),
     )
+    # We call the tool_executor and get back a response
+    response = tool_executor.invoke(action)
+    # We use the response to create a FunctionMessage
+    function_message = FunctionMessage(content=str(response), name=action.tool)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [function_message]}
+
+
+# endregion
 
 
 if "chat_history" not in st.session_state:
@@ -163,104 +224,66 @@ if btn_cols[0].button("清空", key="clear"):
     st.session_state["chat_history"] = []
 
 if btn_cols[1].button("执行", key="run"):
-    llm = ChatVertexAI(
-        # model_name="gemini-1.0-pro-vision-001",
-        model_name="gemini-pro",
+    model = ChatVertexAI(
+        model_name="gemini-1.0-pro-vision-001",
+        # model_name="gemini-pro",
         temperature=0.0,
         max_retries=1,
+        streaming=True,
         convert_system_message_to_human=True,
     )
-    # llm = VertexAI(
-    #     model_name="gemini-1.0-pro-vision-001",
-    #     temperature=0.0,
-    #     max_retries=1,
-    #     convert_system_message_to_human=True,
-    # )
-    # Create the tool
-    search = TavilySearchAPIWrapper()
-    description = """"A search engine optimized for comprehensive, accurate, \
-    and trusted results. Useful for when you need to answer questions \
-    about current events or about recent information. \
-    Input should be a search query. \
-    If the user is asking about something that you don't know about, \
-    you should probably use this tool to see if that can provide any information."""
-    tavily_tool = TavilySearchResults(api_wrapper=search, description=description)
 
-    tools = [tavily_tool]
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            MessagesPlaceholder(variable_name="chat_history"),
-            ("user", "{input}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-    )
     # Function as tool is only supported for `gemini-pro` and `gemini-pro-001` models.
-    llm_with_tools = llm.bind(tools=tools)
+    llm_with_tools = model.bind(tools=tools)
 
-    # agent = (
-    #     {
-    #         "input": lambda x: x["input"],
-    #         "chat_history": lambda x: _format_chat_history(x["chat_history"]),
-    #         "agent_scratchpad": lambda x: format_to_openai_function_messages(
-    #             x["intermediate_steps"]
-    #         ),
-    #     }
-    #     | prompt
-    #     | llm_with_tools
-    #     | OpenAIFunctionsAgentOutputParser()
-    # )
-    # agent = initialize_agent(
-    #     tools,
-    #     llm,
-    #     agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    #     verbose=True,
-    # )
-    # agent = (
-    #     {
-    #         "input": lambda x: x["input"],
-    #         "agent_scratchpad": lambda x: format_to_openai_tool_messages(
-    #             x["intermediate_steps"]
-    #         ),
-    #         "chat_history": lambda x: x["chat_history"],
-    #     }
-    #     | prompt
-    #     | llm_with_tools
-    #     | OpenAIToolsAgentOutputParser()
-    # )
-    agent = (
+    # Define a new graph
+    workflow = StateGraph(AgentState)
+
+    # Define the two nodes we will cycle between
+    workflow.add_node("agent", call_model)
+    workflow.add_node("action", call_tool)
+
+    # Set the entrypoint as `agent`
+    # This means that this node is the first one called
+    workflow.set_entry_point("agent")
+
+    # We now add a conditional edge
+    workflow.add_conditional_edges(
+        # First, we define the start node. We use `agent`.
+        # This means these are the edges taken after the `agent` node is called.
+        "agent",
+        # Next, we pass in the function that will determine which node is called next.
+        should_continue,
+        # Finally we pass in a mapping.
+        # The keys are strings, and the values are other nodes.
+        # END is a special node marking that the graph should finish.
+        # What will happen is we will call `should_continue`, and then the output of that
+        # will be matched against the keys in this mapping.
+        # Based on which one it matches, that node will then be called.
         {
-            "input": lambda x: x["input"],
-            "chat_history": lambda x: _format_chat_history(x["chat_history"]),
-            "agent_scratchpad": lambda x: format_to_openai_function_messages(
-                x["intermediate_steps"]
-            ),
-        }
-        | prompt
-        | llm_with_tools
-        | OpenAIFunctionsAgentOutputParser()
+            # If `tools`, then we call the tool node.
+            "continue": "action",
+            # Otherwise we finish.
+            "end": END,
+        },
     )
+
+    # We now add a normal edge from `tools` to `agent`.
+    # This means that after `tools` is called, `agent` node is called next.
+    workflow.add_edge("action", "agent")
+
+    # Finally, we compile it!
+    # This compiles it into a LangChain Runnable,
+    # meaning you can use it as you would any other runnable
+    app = workflow.compile()
 
     # agent_executor = AgentExecutor.from_agent_and_tools(
     #     agent=agent, tools=tools, verbose=True
     # ).with_types(input_type=AgentInput)
     # agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_types(
-        input_type=AgentInput
-    )
-    # result = agent_executor.invoke(
-    #     {"input": text, "chat_history": st.session_state["chat_history"]}
-    # )
-    result = agent_executor.invoke(
-        {"input": text, "chat_history": st.session_state["chat_history"]}
-    )
-    st.session_state["chat_history"].append(
-        (
-            text,
-            result["output"],
-        )
-    )
-    st.markdown(result["output"])
+    inputs = {"messages": [HumanMessage(content="what is the weather in sf")]}
+    app.invoke(inputs)
+    st.markdown(app.invoke(inputs))
 
 if btn_cols[2].button("graph", key="wiki"):
     from langchain_community.tools import WikipediaQueryRun
